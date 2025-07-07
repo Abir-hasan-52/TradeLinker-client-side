b8081d834c942a0dc13a275d7d76b9a978927962
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-07-08 00:03:29 +0600

    All category components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import WhyChooseUs from '../../Components/ExtraSections/WhyChooseUs';
 import FeaturedBrands from '../../Components/ExtraSections/FeaturedBrands';
 import TopProducts from '../TopProducts/TopProducts';
+import AllCategory from '../TopProducts/AllCategory';
 
 const Home = () => {
     return (
@@ -11,7 +12,12 @@ const Home = () => {
                 <Banner></Banner>
             </section>
             <section>
-                <TopProducts></TopProducts>
+                <h2 className="    text-3xl font-bold text-[#1B365D] mb-6 text-center my-8">All Products</h2>
+                 <p className="text-gray-600 max-w-2xl mx-auto mb-10 text-center">
+                    Explore our wide range of products from verified suppliers. Find the best deals and quality products for your business needs.   
+                </p>
+
+                <AllCategory></AllCategory>
             </section>
             <section>
                 <WhyChooseUs></WhyChooseUs>
---
+++ src/Pages/TopProducts/AllCategory.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const AllCategory = () => {
+    
+  const categories = [
+  {
+    name: "Electronics & Gadgets",
+    image: "https://i.ibb.co/nMFjPjk9/high-angle-view-digital-camera-table-1-1024x683-jpg.webp",
+  },
+  {
+    name: "Home & Kitchen Appliances",
+    image: "https://i.ibb.co/Sw5s8NjV/1690969614632.png",
+  },
+  {
+    name: "Fashion & Apparel",
+    image: "https://i.ibb.co/KjPSN1zW/images-5.jpg",
+  },
+  {
+    name: "Industrial Machinery & Tools",
+    image: "https://i.ibb.co/53QDTQW/machine-tool.jpg",
+  },
+  {
+    name: "Health & Beauty",
+    image: "https://i.ibb.co/4ZWycvqY/health-and-beauty.jpg",
+  },
+  {
+    name: "Automotive Parts & Accessories",
+    image: "https://i.ibb.co/TDtb0F5k/Car-Accessories.webp",
+  },
+  {
+    name: "Office Supplies & Stationery",
+    image: "https://i.ibb.co/b5YWkKx1/shahjahan-brothers-pic-jpg1.jpg",
+  },
+  // add more categories
+];
+    return (
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+            
+      {categories.map((cat) => (
+        <Link key={cat.name} to={`/category/${encodeURIComponent(cat.name)}`}>
+          <div className="shadow rounded overflow-hidden hover:shadow-lg transition">
+            <img src={cat.image} alt={cat.name} className="w-full h-40 object-cover" />
+            <div className="p-3 font-semibold text-center">{cat.name}</div>
+          </div>
+        </Link>
+      ))}
+    </div>
+    );
+};
+
+export default AllCategory;
